0a439323cbe24bd31b586cbdc38bfd4dbd7233bf
Ruan Felipe <44856704+pevifol@users.noreply.github.com>
2019-04-02 10:51:08 -0300

    Create criar vetor.py

---
+++ Projeto da calculadora/criar vetor.py
@@ -0,0 +1,19 @@
+''' Função Criar vetor
+Essa função utiliza uma parametro N para criar um vetor de N coordenadas.
+O resultado é uma lista.
+'''
+def cvec1(x):
+    l=[]
+    p=1
+    while p != x:
+          ṕ+=1
+          l.append(int(input('Digite o valor da '+str(X)+'° coordenada do 1° vetor.')))
+    return l       
+def cvec2(x):
+    c=[]
+    p=1
+    while p != x:
+          ṕ+=1
+          l.append(int(input('Digite o valor da '+str(X)+'° coordenada do 2° vetor.')))
+    return c
+    
