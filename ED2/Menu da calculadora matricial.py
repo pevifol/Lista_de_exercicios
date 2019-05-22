50187a8a8c084817363f138b40db7e813d2be38e
Matheus <48777468+Math0000@users.noreply.github.com>
2019-05-22 20:24:32 -0300

    Update Menu da calculadora matricial.py

--- ED2/Menu da calculadora matricial.py
+++ ED2/Menu da calculadora matricial.py
@@ -255,13 +255,13 @@ def operações():
     print("|                                         |")
     print("+","-"*39,"+")
 
-    opção = int(input(print("Digite o numero da operação que você deseja realizar. ")))
+    opção = int(input("Digite o numero da operação que você deseja realizar. "))
     if opção < 1 or opção > 12: # serve para se a opção digitada não for valida
         print("Essa opção não é valida")
-        a = input(print("Digite 1 para tentar escolher novamente ou digite 2 \n para voltar ao menu inicial. "))
+        a = input("Digite 1 para tentar escolher novamente ou digite 2 \n para voltar ao menu inicial. ")
         while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida
             print("Essa opção não é valida, tenta de novo ai")
-            a= int(input(print("Digite 1 para tentar escolher novamente ou \ndigite 2 para voltar ao menu inicial. ")))
+            a= int(input("Digite 1 para tentar escolher novamente ou \nDigite 2 para voltar ao menu inicial. "))
             
         if a == 1:
             return operações()
@@ -309,19 +309,19 @@ def operações():
 #-----------PODE PARECER DESNECESSARIO, MAS EU FIZ PRA CONCENTRAR EM UM LUGAR SÓ, TODOS OS TRATAMENTOS DE EXCESSÃO----------------
 def escolher(x):
     if x > 0 and x < 4:
-        a = int(input(print("Digite 1 para escolher matriz 1 da memoria ou\n digite 2 para digitar matriz 1. ")))
+        a = int(input("Digite 1 para escolher matriz 1 da memoria ou \nDigite 2 para digitar matriz 1. "))
         while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida
             print("Essa opção não é valida, tenta de novo ai")
-            a= int(input(print("Digite 1 para escolher matriz 1 da memoria ou\n digite 2 para digitar matriz 1. ")))
+            a = int(input("Digite 1 para escolher matriz 1 da memoria ou \nDigite 2 para digitar matriz 1. "))
         if a == 1:
             mat1 = lermatriz()
         else:
             mat1 = matriz_criar()
 
-        a = int(input(print("Digite 1 para escolher matriz 2 da memoria ou\n digite 2 para digitar matriz 2. ")))
+        a = int(input("Digite 1 para escolher matriz 2 da memoria ou \nDigite 2 para digitar matriz 2. "))
         while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida ( é o unico jeito que eu sei )
             print("Essa opção não é valida, tenta de novo ai")
-            a = int(input(print("Digite 1 para escolher matriz 2 da memoria ou\n digite 2 para digitar matriz 2. ")))
+            a = int(input("Digite 1 para escolher matriz 2 da memoria ou \nDigite 2 para digitar matriz 2. "))
         if a == 1:
             mat2 = lermatriz()
         else:
@@ -330,10 +330,10 @@ def escolher(x):
         if x == 1 or x == 2:
             if len(mat1)!=len(mat2) or len(mat1[0])!=len(mat2[0]):
                 print('Impossível realizar operação, pois suas matrizes não possuem o mesmo número de linhas ou colunas.')
-                a = input(print("Digite 1 para escolher outras matrizes ou digite 2 para retornar ao menu inicial. "))
+                a = int(input("Digite 1 para escolher outras matrizes ou \nDigite 2 para retornar ao menu inicial. "))
                 while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida ( é o unico jeito que eu sei )
                     print("Essa opção não é valida, tenta de novo ai")
-                    a = int(input(print("Digite 1 para escolher outras matrizes ou digite 2 para retornar ao menu inicial. ")))
+                    a = int(input("Digite 1 para escolher outras matrizes ou \nDigite 2 para retornar ao menu inicial. "))
                 if a == 1:
                     final = escolhe(x)
                 else:
@@ -343,11 +343,11 @@ def escolher(x):
 
         else:
             if len(mat1[0])!=len(mat2):
-                print('Impossível realizar operação,pois a matriz 1 não possui o\n numero de colunas igual ao numero de linhas da matriz 2.')
-                a = input(print("Digite 1 para escolher outras matrizes ou digite 2 para retornar ao menu inicial. "))
+                print('Impossível realizar operação,pois a matriz 1 não possui o \nnumero de colunas igual ao numero de linhas da matriz 2.')
+                a = int(input("Digite 1 para escolher outras matrizes ou \n Digite 2 para retornar ao menu inicial. "))
                 while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida ( é o unico jeito que eu sei )
                     print("Essa opção não é valida, tenta de novo ai")
-                    a = int(input(print("Digite 1 para escolher outras matrizes ou digite 2 para retornar ao menu inicial. ")))
+                    a = int(input("Digite 1 para escolher outras matrizes ou \nDigite 2 para retornar ao menu inicial. "))
                 if a == 1:
                     final = escolhe(x)
                 else:
@@ -355,13 +355,12 @@ def escolher(x):
             else:
                 final = [mat1,mat2]
                 
-        return (final)
 
     elif x > 3 and x < 8:
-        a = int(input(print("Digite 1 para escolher matriz da memoria ou\n digite 2 para digitar matriz. ")))
+        a = int(input("Digite 1 para escolher matriz da memoria ou \nDigite 2 para digitar matriz. "))
         while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida
             print("Essa opção não é valida, tenta de novo ai")
-            a= int(input(print("Digite 1 para escolher matriz da memoria ou\n digite 2 para digitar matriz. ")))
+            a = int(input("Digite 1 para escolher matriz da memoria ou \nDigite 2 para digitar matriz. "))
         if a == 1:
             mat1 = lermatriz()
         else:
@@ -370,10 +369,10 @@ def escolher(x):
         if x == 4 or x == 6:
             if len(mat1) < 2:
                 print('Impossível realizar operação, pois suas matriz não possui linhas suficientes. ')
-                a = input(print("Digite 1 para escolher outra matriz ou digite 2 para retornar ao menu inicial. "))
+                a = int(input("Digite 1 para escolher outras matriz ou \nDigite 2 para retornar ao menu inicial. "))
                 while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida ( é o unico jeito que eu sei )
                     print("Essa opção não é valida, tenta de novo ai")
-                    a = int(input(print("Digite 1 para escolher outra matriz ou digite 2 para retornar ao menu inicial. ")))
+                    a = int(input("Digite 1 para escolher outras matriz ou \nDigite 2 para retornar ao menu inicial. "))
                 if a == 1:
                     final = escolhe(x)
                 else:
@@ -384,10 +383,10 @@ def escolher(x):
         else:
             if len(mat1[0]) < 2:
                 print('Impossível realizar operação, pois suas matriz não possui colunas suficientes. ')
-                a = input(print("Digite 1 para escolher outra matriz ou digite 2 para retornar ao menu inicial. "))
+                a = int(input("Digite 1 para escolher outras matriz ou \nDigite 2 para retornar ao menu inicial. "))
                 while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida ( é o unico jeito que eu sei )
                     print("Essa opção não é valida, tenta de novo ai")
-                    a = int(input(print("Digite 1 para escolher outra matriz ou digite 2 para retornar ao menu inicial. ")))
+                    a = int(input("Digite 1 para escolher outras matriz ou \nDigite 2 para retornar ao menu inicial. "))
                 if a == 1:
                     final = escolhe(x)
                 else:
@@ -395,29 +394,65 @@ def escolher(x):
             else:
                 final = mat1
                 
-        return (final)
 
-    else:
-        a = int(input(print("Digite 1 para escolher matriz da memoria ou\n digite 2 para digitar matriz. ")))
+    elif x > 7 and x < 12:
+        a = int(input("Digite 1 para escolher matriz da memoria ou \nDigite 2 para digitar matriz. "))
         while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida
             print("Essa opção não é valida, tenta de novo ai")
-            a= int(input(print("Digite 1 para escolher matriz da memoria ou\n digite 2 para digitar matriz. ")))
+            a = int(input("Digite 1 para escolher matriz da memoria ou \nDigite 2 para digitar matriz. "))
+        if a == 1:
+            mat1 = lermatriz()
+        else:
+            mat1 = matriz_criar()
+       
+        final = mat1
+
+    else:
+        a = int(input("Digite 1 para escolher matriz da memoria ou \nDigite 2 para digitar matriz. "))
+        while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida
+            print("Essa opção não é valida, tenta de novo ai")
+            a = int(input("Digite 1 para escolher matriz da memoria ou \nDigite 2 para digitar matriz. "))
         if a == 1:
             mat1 = lermatriz()
         else:
             mat1 = matriz_criar()
 
-        return (final)
+        if len(mat1) != 2 and len(mat1[0]) != 2:
+            print('Impossível realizar operação, pois suas matriz não é do tipo 2x2. ')
+            a = int(input("Digite 1 para escolher outras matriz ou \nDigite 2 para retornar ao menu inicial. "))
+            while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida ( é o unico jeito que eu sei )
+                print("Essa opção não é valida, tenta de novo ai")
+                a = int(input("Digite 1 para escolher outras matriz ou \nDigite 2 para retornar ao menu inicial. "))
+            if a == 1:
+                final = escolhe(x)
+            else:
+                return menu()
+        
+        elif(float(mat[0][0]) * float(mat[1][1]) - float(mat[0][1]) * float(mat[1][0])) == 0 :
+            print('Impossível realizar operação, pois suas matriz possui determinante \nigual a 0, logo ela não é invertivel. ')
+            a = int(input("Digite 1 para escolher outras matriz ou \nDigite 2 para retornar ao menu inicial. "))
+            while a!=1 and a!=2:# aqui é pra garantir que digite uma opção valida ( é o unico jeito que eu sei )
+                print("Essa opção não é valida, tenta de novo ai")
+                a = int(input("Digite 1 para escolher outras matriz ou \nDigite 2 para retornar ao menu inicial. "))
+            if a == 1:
+                final = escolhe(x)
+            else:
+                return menu()
+        else:
+            final = mat1
+        
+    return final
+        
 #-----------------------ESSA FUNÇÃO AQUI É SÓ PRO CARA DIGITAR A MATRIZ DA OPERAÇÃO, MAS É DIFERENTE DO CRIAR_MATRIZ------------------
 #---------------------------PORQUE NÃO DEIXA O USUARIO SALVAR A MATRIZ CRIADA NA MEMORIA, É SÓ PRA USAR NA CONTA---------------------
 def matriz_criar():
-    m=int(input(print("Sua matriz possui quantas linhas? ")))
-    n=int(input(print("Sua matriz possui quantas colunas? ")))
+    m=int(input("Sua matriz possui quantas linhas? "))
+    n=int(input("Sua matriz possui quantas colunas? "))
     mat=[]
     for i in range(m):
         linha=[]
         for j in range(n):
-            a = float(input(print("Qual o elemento da {0}° linha, {1}° coluna? ".format(i,j))))
+            a = float(input("Qual o elemento da {0}° linha, {1}° coluna? ".format(i,j)))
             lista.append(a)
         mat.append(linha)
     
@@ -432,11 +467,12 @@ def matriz_criar():
         print(" |")
     print("+-"," "*(7*len(mat[0])-2),"-+")
     
-    resp = input(print("Digite 1 se a matriz estiver certa ou \nDigite 2 para digitar novamente. "))
+    resp = int(input("Digite 1 se a matriz estiver certa ou \nDigite 2 para digitar novamente. "))
     while resp!=1 and resp!=2:# aqui é pra garantir que digite uma opção valida
         print("Essa opção não é valida, tenta de novo ai")
-        resp = int(input(print("Digite 1 se a matriz estiver certa ou \nDigite 2 para digitar novamente. ")))
+        resp = int(input("Digite 1 se a matriz estiver certa ou \nDigite 2 para digitar novamente. "))
     if resp == 1 :
+        
         print("Tudo bem")
         return mat
                 
@@ -444,7 +480,7 @@ def matriz_criar():
         print("Tudo bem")
         return matriz_criar()   
     
-#-------ESSA AQUI EU CRIEI SÓ PRA PRINTAR O RESULTADO QUE SAI DE UMA OPERAÇÃO E PERGUNTAR SE O USUARIO QUER SALVAR O RESULTADO-------
+#-------ESSA AQUI É SÓ PRA PRINTAR O RESULTADO QUE SAI DE UMA OPERAÇÃO E PERGUNTAR SE O USUARIO QUER SALVAR O RESULTADO-------
 def resultado(x):
     
     print("O resultado é\n")
@@ -458,19 +494,20 @@ def resultado(x):
         print(" |")
     print("+-"," "*(7*len(x[0])-2),"-+")
 
-    sal = input(print("Digite 1 para salvar o resultado \nDigite 2 para retornar ao menu de operações ou \nDigite 3 para retornar ao menu inicial. "))
+    sal = int(input("Digite 1 para salvar o resultado, \nDigite 2 para retornar ao menu de operações ou \nDigite 3 para retornar ao menu inicial. "))
         while sal!=1 and sal!=2 and sal!=3:# aqui é pra garantir que digite uma opção valida
             print("Essa opção não é valida, tenta de novo ai")
-            sal = int(input(print("Digite 1 para salvar o resultado \nDigite 2 para retornar ao menu de operações ou \nDigite 3 para retornar ao menu inicial. ")))
+            sal = int(input("Digite 1 para salvar o resultado, \nDigite 2 para retornar ao menu de operações ou \nDigite 3 para retornar ao menu inicial. "))
     if sal == 1:
         salvarmatriz(x):
         return menu()
     
     elif sal == 2:
         return operações()
+    
     else:
         return menu()
-#-----------------------------AQUI SÃO TODAS AS OPERAÇÕES PRONTAS ( TIRANDO A INVERSÃO 2X2 )--------------------------------
+#--------------------------------------------------AQUI SÃO TODAS AS OPERAÇÕES PRONTAS --------------------------------
 def soma_subt(x,y,z):
     m = int(len(x))
     n = int(len(x[0]))
@@ -513,8 +550,8 @@ def som_lin_col(x,y):
     else:
         pa = 'coluna'
             
-    l1 = int(input(print('Digite o numero da {} que em que o resultado permanecera. '.format(pa))))
-    l2 = int(input(print('Digite o numero da {} que você somara a linha escolhida a cima. '.format(pa))))
+    l1 = int(input('Digite o numero da {} que em que o resultado permanecera. '.format(pa)))
+    l2 = int(input('Digite o numero da {} que você somara a linha escolhida a cima. '.format(pa)))
     for i in range(n):
         linha=[]
         if i == (l1-1):
@@ -550,8 +587,8 @@ def per_lin_col(x,y):
         m = int(len(x[0]))
         n = m
         
-    l1 = int(input(print('Digite o numero de uma das {}s que você quer permutar. '.format(pa))))
-    l2 = int(input(print('Digite o numero da outra {} que você quer permutar. '.format(pa))))
+    l1 = int(input('Digite o numero de uma das {}s que você quer permutar. '.format(pa)))
+    l2 = int(input('Digite o numero da outra {} que você quer permutar. '.format(pa)))
     for i in range(n):
         linha=[]
         if i == (l1-1):
@@ -623,7 +660,7 @@ def multi_lin_col_esc(x,y):
         m = int(len(x[0]))
         n = m
     k=int(input('Digite o valor pelo qual deseja que a {} da matriz seja multiplicada. '.format(pa)))
-    l1 = int(input(print('Digite o numero da {0} que você quer multiplicar por {1}. '.format(pa,k))))
+    l1 = int(input('Digite o numero da {0} que você quer multiplicar por {1}. '.format(pa,k)))
     for i in range(n):
         linha=[]
         if i == (l1-1):
@@ -641,9 +678,27 @@ def multi_lin_col_esc(x,y):
             linha.append(p)   
         mat.append(linha)
     
-    if y == 6:
+    if y == 10:
         return multi_lin_col_esc(mat,x)
     else:
         return multi_lin_col_esc(x,mat) 
 #-----------------------------------------------------------------------------------------------------------------
 def inversão(x):
+    a = float(x[0][0])
+    b = float(x[0][1])
+    c = float(x[1][0])
+    d = float(x[1][1])
+    det = (a*d - b*c)
+    
+    linha1 = []
+    linha2 = []   
+    mat = []
+    
+    linha1.append(d/det)
+    linha1.append((-b)/det)
+    mat.append(linha1)
+    linha2.append((-c)/det)
+    linha2.append(a/det)
+    mat.append(linha2)
+
+   return resultado(mat)
