b14785ee94d819e31553cf5d7a6efa9dc029e89b
Ruan Felipe <ruanfelipe2010@gmail.com>
2019-07-24 11:22:11 -0300

    ED04 concluído.
    um pesadelo técnico, mas não tinhamos muita escolha.

---
+++ ED04/ED04.py
@@ -0,0 +1,24 @@
+%matplotlib inline
+import pandas as pd
+dado=pd.read_csv(input('Digite aqui o path para o arquivo: '),engine='c',encoding="latin1",sep=";")
+#Código originalmente testado somente com 300.000 linhas, devido a incapacidade do jupyter de utilizar o banco de dados inteiro.
+#(O jupyter crashava repetidamente na hora de fazer os gráficos utilizando o conjunto de dados inteiro)
+#Isso significa que algumas excessões provavelmente não vão poder ser tratadas, justamente pois elas não aconteceram
+#durante o desenvolvimento :/ Tentamos remediar isso por meio do leia-me, mas ele não lista todas as possibilidades
+#nos campos.
+dado=dado[dado.DS_FAIXA_ETARIA != "Inválido"]
+dado=dado[dado.SG_UF != 'ZZ']
+dado=dado[dado.SG_UF != 'VT']
+dado=dado[dado.SG_UF != 'BR']
+dado=dado[dado.DS_GENERO != "NÃO INFORMADO"]
+dado=dado[dado.DS_GRAU_ESCOLARIDADE != "NÃO INFORMADO"]
+#Todas os "problemas" encontrados durante o teste.
+a=dado.groupby(['DS_FAIXA_ETARIA','DS_GENERO']).sum().unstack().plot(kind='bar',y='QT_ELEITORES_PERFIL',title='Gráfico Faixa etária x Eleitores')
+a.set_xlabel("Faixa etária")
+a.set_ylabel("N° de eleitores")
+b=dado.groupby(['SG_UF','DS_GENERO']).sum().unstack().plot(kind='bar',y='QT_ELEITORES_PERFIL',title='Gráfico UF x Eleitores')
+b.set_xlabel("Unidade Federativa")
+b.set_ylabel("N° de eleitores")
+c=dado.groupby(['DS_GRAU_ESCOLARIDADE','DS_GENERO']).sum().unstack().plot(kind='bar',y='QT_ELEITORES_PERFIL',title='Gráfico Escolaridade x Eleitores')
+c.set_xlabel("Grau de escolaridade")
+c.set_ylabel("N° de eleitores")
---
+++ ED04/README.md
@@ -0,0 +1,7 @@
+#ED04
+Feito quase que totalmente no Jupyter.
+Grupo:
+1) Ruan felipe da Silva e sousa
+2) Felipe Chen wu
+3) Mariana Lima
+
