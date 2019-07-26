6eb9bb51ab2a30e614894b0765e5510f08055a0b
Ruan Felipe <44856704+pevifol@users.noreply.github.com>
2019-07-26 08:38:30 -0300

    "Ela é amiga da minha mulher
    pois é, pois é
    mas vive dando em cima de mim
    enfim, enfim"
    agora salva os gráficos

--- ED04/ED04.py
+++ ED04/ED04.py
@@ -1,4 +1,5 @@
 %matplotlib inline
+import mathplotlib.pyplot as plt
 import pandas as pd
 dado=pd.read_csv(input('Digite aqui o path para o arquivo: '),engine='c',encoding="latin1",sep=";")
 #Código originalmente testado somente com 300.000 linhas, devido a incapacidade do jupyter de utilizar o banco de dados inteiro.
@@ -16,9 +17,14 @@ dado=dado[dado.DS_GRAU_ESCOLARIDADE != "NÃO INFORMADO"]
 a=dado.groupby(['DS_FAIXA_ETARIA','DS_GENERO']).sum().unstack().plot(kind='bar',y='QT_ELEITORES_PERFIL',title='Gráfico Faixa etária x Eleitores')
 a.set_xlabel("Faixa etária")
 a.set_ylabel("N° de eleitores")
+plt.savefig('gráficofxetaria.png')
+
 b=dado.groupby(['SG_UF','DS_GENERO']).sum().unstack().plot(kind='barh',y='QT_ELEITORES_PERFIL',title='Gráfico UF x Eleitores')
 b.set_xlabel("Unidade Federativa")
 b.set_ylabel("N° de eleitores")
+plt.savefig('gráficogenero.png')
+
 c=dado.groupby(['DS_GRAU_ESCOLARIDADE','DS_GENERO']).sum().unstack().plot(kind='bar',y='QT_ELEITORES_PERFIL',title='Gráfico Escolaridade x Eleitores')
 c.set_xlabel("Grau de escolaridade")
 c.set_ylabel("N° de eleitores")
+plt.savefig('gráficoescola.png')
